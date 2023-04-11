e9b7783a11b470786517de961fbffb65167d3a27
Hirsh1107 <46503729+Hirsh1107@users.noreply.github.com>
2023-04-11 01:43:37 -0500

    Add files via upload

---
+++ Assignment02/react-app/src/Products.js
@@ -0,0 +1,45 @@
+export const Products = [
+    {
+        "id": 1,
+        "title": "product 1",
+        "price": 40,
+        "description": "this is product 1",
+        "img_src": ""
+    },
+    {
+        "id": 2,
+        "title": "product 2",
+        "price": 40,
+        "description": "this is product 2",
+        "img_src": ""
+    },
+    {
+        "id": 3,
+        "title": "product 3",
+        "price": 40,
+        "description": "this is product 3",
+        "img_src": ""
+    },
+    {
+        "id": 4,
+        "title": "product 4",
+        "price": 40,
+        "description": "this is product 4",
+        "img_src": ""
+    },
+    {
+        "id": 5,
+        "title": "product 5",
+        "price": 40,
+        "description": "this is product 5",
+        "img_src": ""
+    },
+    {
+        "id": 6,
+        "title": "product 6",
+        "price": 40,
+        "description": "this is product 6",
+        "img_src": ""
+    }
+
+]
--- Assignment02/react-app/src/index.js
+++ Assignment02/react-app/src/index.js
@@ -1,62 +1,69 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Products } from './Products';
+
 
 function App() {
+  const [ProductsCategory, setProductsCategory] = useState(Products);
   const [searchTerm, setSearchTerm] = useState('');
+  let [count, setCount] = useState(0);
 
   function handleSearchChange(event) {
     setSearchTerm(event.target.value);
+    const results = ProductsCategory.filter(eachProduct => {
+      if (event.target.value === "") return ProductsCategory;
+      return eachProduct.title.toLowerCase().includes(event.target.value.toLowerCase())
+    });
+    setProductsCategory(results);
   }
 
   function handleSearchSubmit(event) {
     event.preventDefault();
     // Perform search logic here
+
+  }
+
+  function increment() {
+    count++;
+    setCount(count);
+  }
+  function decrement() {
+    count--;
+    if(count < 0) {
+      count = 0;
+    }
+    setCount(count);
+  }
+
+  function quantity_picker() {
+    return (
+      <div>
+        <p>{count}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    )
   }
 
   return (
     <div>
       <h1>Store Assignment 2</h1>
+      <h2>Products: {Products.length}</h2>
       <form onSubmit={handleSearchSubmit}>
         <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Search products" />
         <button type="submit">Search</button>
       </form>
       <div className="grid-container">
-        <div className="grid-item">
-          <img src="" alt="Product 1" />
-          <h2>Product 1</h2>
-          <p>Price</p>
+        {ProductsCategory.map((product, i) => (
+          <div className="grid-item">
+          <img src={product.img_src} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>${product.price}</p>
+          {quantity_picker()}
           <button>Add to Cart</button>
         </div>
-        <div className="grid-item">
-          <img src="" alt="Product 2" />
-          <h2>Product 2</h2>
-          <p>Price</p>
-          <button>Add to Cart</button>
-        </div>
-        <div className="grid-item">
-          <img src="" alt="Product 3" />
-          <h2>Product 3</h2>
-          <p>Price</p>
-          <button>Add to Cart</button>
-        </div>
-        <div className="grid-item">
-          <img src="" alt="Product 4" />
-          <h2>Product 4</h2>
-          <p>Price</p>
-          <button>Add to Cart</button>
-        </div>
-        <div className="grid-item">
-          <img src="" alt="Product 5" />
-          <h2>Product 5</h2>
-          <p>$Price</p>
-          <button>Add to Cart</button>
-        </div>
-        <div className="grid-item">
-          <img src="" alt="Product 6" />
-          <h2>Product 6</h2>
-          <p>Price</p>
-          <button>Add to Cart</button>
-        </div>
+        ))}
       </div>
       <button>Shop Now</button>
     </div>
