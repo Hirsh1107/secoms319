36a667ebf999e3497c22fb1f48495d03533990a0
JJiang76 <jasonjiang27@gmail.com>
2023-04-27 12:29:20 -0500

    Assignment03 setup

--- Assignment03/frontend/src/App.js
+++ Assignment03/frontend/src/App.js
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [product, setProduct] = useState([]);
-  const [viewer1, setViewer1] = useState(false);
   const [view, setView] = useState(1); // 1 = create, 2 = read, 3 = update, 4 = delete
 
   function handleViewChange(viewnum) {
     setView(viewnum)
   }
 
+  //handle all views here
   function getView() {
     switch (view) {
       case 1: 
@@ -18,23 +18,28 @@ function App() {
       
       case 2: 
         return (
-          <h1>A view to show or Read all the products contained in your catalog in Mongo</h1>
+          <h1>2: A view to show or Read all the products contained in your catalog in Mongo</h1>
         );
 
       case 3:
         return (
-          <h1>A view to Update the price for any product already contained in the catalog in Mongo</h1>
+          <h1>3: A view to Update the price for any product already contained in the catalog in Mongo</h1>
         );
 
       case 4: 
         return (
-          <h1>A view to Delete one product from the catalog in Mongo</h1>
+          <h1>4: A view to Delete one product from the catalog in Mongo</h1>
         );
     }
   }
 
   return (
     <div>
+      <h1>Assignment03</h1>
+      <button onClick={() => handleViewChange(1)}>Create</button>
+      <button onClick={() => handleViewChange(2)}>Read</button>
+      <button onClick={() => handleViewChange(3)}>Update</button>
+      <button onClick={() => handleViewChange(4)}>Delete</button>
       {getView()};
     </div>
   );
