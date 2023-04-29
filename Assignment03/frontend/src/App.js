517e7384750a90de3116e9aeeeefa693dd4f3d5f
Hirsh1107 <46503729+Hirsh1107@users.noreply.github.com>
2023-04-28 20:40:20 -0500

    Add files via upload

--- Assignment03/frontend/src/App.js
+++ Assignment03/frontend/src/App.js
@@ -8,12 +8,70 @@ function App() {
     setView(viewnum)
   }
 
+  function Form() {
+    const [formData, setFormData] = useState({
+      _id: '',
+      title: '',
+      price: '',
+      description: '',
+      category: '',
+      image: '',
+      rate: 0,
+      count: 0
+    });
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      alert(formData._id)
+    }
+
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+
+    function FormElement(props) {
+      return (
+        <div>
+          {props.text}:
+          <input 
+            type="text" 
+            id = {props.text}
+            value = {props.val}
+            onChange={handleInputChange}
+          />
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <h1>Post new item</h1>
+        <form onSubmit={handleSubmit}>
+          <FormElement text={"_id"} val={formData._id}/>
+          <FormElement text={"title"} val={formData.title}/>
+          <FormElement text={"price"}  val={formData.price}/>
+          <FormElement text={"description"} val={formData.description}/>
+          <FormElement text={"category"} val={formData.category}/>
+          <FormElement text={"image"} val={formData.image}/>
+          <FormElement text={"rate"} val={formData.rate}/>
+          <FormElement text={"count"} val={formData.count}/>
+        <input type="submit" />
+        </form>
+      </div>
+        
+    )
+  }
+
   //handle all views here
   function getView() {
     switch (view) {
       case 1: 
         return (
-          <h1>1: A view to add or Create a new product to the database in Mongo</h1>
+          <Form/>
         );
       
       case 2: 
