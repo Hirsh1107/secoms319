76c919987121953fc0cdda03f5614e59e58b0c7b
Hirsh1107 <46503729+Hirsh1107@users.noreply.github.com>
2023-04-28 20:40:43 -0500

    Add files via upload

--- Assignment03/backend/index.js
+++ Assignment03/backend/index.js
@@ -19,3 +19,25 @@ const host = "localhost";
 app.listen(port, () => {
     console.log(`App listening at http://%s:%s`, host, port);
 });
+
+app.get("/", async (req, resp) => {
+    const query = {};
+    const allProducts = await Product.find(query);
+    console.log(allProducts);
+    resp.send(allProducts);
+});
+
+function postMethod(postData) {
+    fetch('http://localhost:8081/addUser', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ postData })
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+    var container = document.getElementById("showData");
+    container.innerHTML = JSON.stringify(data);
+    });
+}    
+    
